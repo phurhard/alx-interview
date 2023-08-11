63eef343038314b3e85361346e56a460bdf82bb1
phurhard <phurhardeen@gmail.com>
2023-08-11 17:14:22 +0100

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Minimum no of operations that can be carried out on a file"""
+
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    factors = []
+
+    # Find prime factors of n
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:
+        factors.append(n)
+
+    if not factors:
+        return 0
+
+    return sum(factors)
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This is a project repositiories for technical interviews questions
 It is to help improve software development skills
 Contains the following folders
 1. Pascal Triangle
+2. LockBoxes
+3. Minimum Operation
