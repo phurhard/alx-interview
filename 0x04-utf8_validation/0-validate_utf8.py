036791c1e38c15a3c90b9583ad5b3c9ebb03a48f
phurhard <phurhardeen@gmail.com>
2023-08-27 04:11:24 +0100

    utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" UTF-8 Validation"""
+import codecs
+
+
+def validUTF8(data):
+    """ This function checkes where the passed
+    list of integers are correct utf8 values"""
+
+    # convert list of data to bytes
+    try:
+        dataBytes = bytes(data)
+        # decode the bytes using utf8 encoding
+        decodedData = dataBytes.decode('utf-8')
+        return True
+    except Exception as e:
+        return False
