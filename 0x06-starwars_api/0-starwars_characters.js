05ae18e37d1069a8df98fa2ae628809b5234d505
phurhard <phurhardeen@gmail.com>
2023-09-10 21:35:57 +0100

    semist dard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,12 +3,14 @@ const request = require('request');
 const movie = process.argv[2];
 request(`https://swapi-api.alx-tools.com/api/films/${movie}`,
   function (err, res, body) {
+    console.log(err);
     body = JSON.parse(body);
-	  const characters = body.characters
-	  for (const character of characters) {
-		  request(character, function(err, res, body) {
-			  person = JSON.parse(body)
-			  console.log(person.name)
-		  })
-	  }
+    const characters = body.characters;
+    for (const character of characters) {
+      request(character, function (error, res, body) {
+        console.log(error);
+        const person = JSON.parse(body);
+        console.log(person.name);
+      });
+    }
   });
