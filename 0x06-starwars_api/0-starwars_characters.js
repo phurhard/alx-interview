01202a65badf3a95a0aedb0a43aaa64e28e494fb
phurhard <phurhardeen@gmail.com>
2023-09-11 01:18:19 +0100

    starwars1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,52 @@
 #!/usr/bin/node
 const request = require('request');
 const movie = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movie}`;
+/**
+ * While this code works fine, it does not take into consideration the format at which the names are placed on the endpoint
 request(`https://swapi-api.alx-tools.com/api/films/${movie}`,
   function (err, res, body) {
     console.log(err);
     body = JSON.parse(body);
     const characters = body.characters;
+    console.log(characters)
     for (const character of characters) {
+      console.log(character)
       request(character, function (error, res, body) {
-        console.log(error);
+        if (error) {return error;}
         const person = JSON.parse(body);
-        console.log(person.name);
+//        console.log(person.name);
       });
     }
   });
+**/
+
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      //    console.log('makeRequest', url)
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+async function fetchData () {
+  try {
+    const data = await makeRequest(url);
+    const dataJson = JSON.parse(data);
+    const characters = dataJson.characters;
+    for (const character of characters) {
+      const Data = await makeRequest(character);
+      const people = JSON.parse(Data);
+      console.log(people.name);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+fetchData();
