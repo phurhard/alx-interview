9a96c8a4d11bce2f20ad8937f5feedc8b69ed578
phurhard <phurhardeen@gmail.com>
2024-01-04 12:15:52 +0100

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,10 +8,12 @@ def validUTF8(data):
     list of integers are correct utf8 values"""
 
     # convert list of data to bytes
+    if len(data) == 0:
+        return False
     try:
         dataBytes = bytes(data)
         # decode the bytes using utf8 encoding
-        decodedData = dataBytes.decode('utf-8')
+        dataBytes.decode('utf-8')
         return True
     except Exception as e:
         return False
