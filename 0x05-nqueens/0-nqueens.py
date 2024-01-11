697fc7be50d870204dbf6d438631e1b2020987a5
phurhard <phurhardeen@gmail.com>
2024-01-11 21:26:41 +0100

    NQueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,35 +4,48 @@ import sys
 
 
 def queens():
-    """The function"""
+    """The queens function"""
+    # the checks
+    if len(sys.argv) > 2 or len(sys.argv) == 1:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    n = sys.argv[1]
     try:
-        if len(sys.argv) > 2:
-            print('Usage: nqueens N\n')
-            sys.exit(1)
-        n = sys.argv[1]
-        try:
-            n = int(n)
-        except Exception:
-            print('N must be a number\n')
-            sys.exit(1)
-        if n < 4:
-            print('N must be at least 4\n')
-            sys.exit(1)
-        else:
-            print('Got here')
-    except Exception as e:
-        print(f'Exception got here as {e}')
-        sys.exit(0)
+        n = int(n)
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+    # create a matrix/board
+    matrix = [[0] * n for _ in range(n)]
+    COLUMNS = set()
+    POSDIA = set()
+    NEGDIA = set()
 
-# queens()
-def Queens(n, i, a, b, c):
-    if i < n:
-        for j in range(n):
-            if j not in a and i+j not in b and i-j not in c:
-                yield from Queens(n, i+1, a+[j], b+[i+j], c+[i-j])
-    else:
-        yield a
+    def backtrack(r):
+        """The backtracking is sweet"""
+        if r < n:
+            for c in range(n):
+                if c in COLUMNS or (r + c) in POSDIA or (r - c) in NEGDIA:
+                    continue
+                COLUMNS.add(c)
+                POSDIA.add(r + c)
+                NEGDIA.add(r - c)
+                matrix[r][c] = 'Q'
+                backtrack(r + 1)
+                COLUMNS.remove(c)
+                POSDIA.remove(r + c)
+                NEGDIA.remove(r - c)
+                matrix[r][c] = '0'
+        if r == n:
+            # print(matrix)
+            ans = [[row, c] for row in range(n) for c in range(n)
+                   if matrix[row][c] == 'Q']
+            print(ans)
+            return
+    backtrack(0)
 
-for solution in Queens(6, 0, [], [], []):
-    print(solution)
 
+queens()
--- 0x05-nqueens/README.md
+++ 0x05-nqueens/README.md
@@ -1,2 +1,27 @@
-NQueens
-chess
+## NQueens
+
+What does the nqueens problem postulate??
+
+The nqueens problem postulates a chess game problem where by N number of Queens are placed on a NxN board without any of the queens attacking each other
+
+## Important Notes
+
+* N number of Queens
+* NxN board
+* No two Queen should be able to attack each other
+
+In chess, a Queen can move along any path on the chessboard as she is the most powerful piece of the game
+She can move verticaly, horizontally, positive diagonally(bottom left to top right) or negative diagonally(top left to bottom right)
+Hence a way to achieve the NQueens factor is to ensure that
+
+    Each Queen is on it's own row and column and also ensure it is also on it's own positive diagonal and negative diagonal.
+
+For the row and column it's quite easier with a for loop but the diagonals are a little bit tricky.
+The formula to use for the diagonal are given below
+
+    for the negative diagonal r-c = constant {from top left to bottom right}
+    for the positive diagonal r+c = constant {bottom left to top right}
+
+after these the board is transversed and the cells position values are returned
+
+
