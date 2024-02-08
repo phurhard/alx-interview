7fb6a2a6110a6220e6f34cf14a63fb81f44e9be9
phurhard <phurhardeen@gmail.com>
2024-02-08 19:17:11 +0100

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,14 +3,22 @@
 
 
 def island_perimeter(grid):
-    """Island Perimeter calculation"""
-
-    row_count = set()
-    coloum_count = set()
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    # rowC = set()
+    # colC = set()
+    perimeter = 0
     for r in range(len(grid)):
-        for c in range(len(grid[r])):
+        for c in range(len(grid[0])):
             if grid[r][c] == 1:
-                row_count.add(r)
-                coloum_count.add(c)
-    row = len(row_count) + len(coloum_count)
-    return 2 * row
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c-1] == 1:
+                    perimeter -= 2
+                # rowC.add(r)
+                # colC.add(c)
+    # peri = len(rowC) + len(colC)
+    return perimeter
