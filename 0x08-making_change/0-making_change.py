41add1e44ba05e0bb9cffb73cbb79fee9ee113a5
phurhard <phurhardeen@gmail.com>
2024-02-09 12:04:11 +0100

    making change ND

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,3 +4,10 @@
 
 def makeChange(coins, total):
     """Making change of total, given list of coins"""
+    min_coins = -1
+    solution = []
+    coins_used = 1
+    if total == 0 or total < 0:
+        return 0
+    for coin in coins:
+        pass
