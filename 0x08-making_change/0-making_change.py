cf19e0e0b809affef61f8b058da56167d449323e
phurhard <phurhardeen@gmail.com>
2024-02-09 21:33:15 +0100

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,12 +2,16 @@
 """Making change LeetCode"""
 
 
-def makeChange(coins, total):
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
     """Making change of total, given list of coins"""
-    min_coins = -1
-    solution = []
-    coins_used = 1
-    if total == 0 or total < 0:
-        return 0
-    for coin in coins:
-        pass
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+    return dp[total] if dp[total] != total + 1 else -1
